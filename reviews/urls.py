2dfcb759a2ec4288c5460b971acc140ed1c96fd5
Guy Steele-Perkins <guysteeleperkins@hotmail.com>
2024-11-25 17:45:19 +0000

    added review detail

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='reviews'),
-    # path('<int:pk>', views.review_detail, name='review_detail'),
+    path('<int:pk>', views.review_detail, name='review_detail'),
 ]
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Review
 # Create your views here.
@@ -6,3 +6,20 @@ from .models import Review
 class ReviewList(generic.ListView):
     model = Review
     template_name = 'reviews/reviews.html'
+    paginate_by = 8
+
+def review_detail(request, pk):
+    """
+    Display an individual :model:`review.Review`.
+
+    **Context**
+
+    ``review``
+        An instance of :model:`review.Review`.
+
+    **Template:**
+
+    :template:`reviews/review_detail.html`
+    """
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, 'reviews/review_detail.html', {'review': review})
